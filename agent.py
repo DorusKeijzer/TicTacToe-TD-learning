c96e291158ad38b2498776bea3cc87417f5ad90b
Dorus <doruskeijzer@gmail.com>
2024-11-16 08:31:41 +0100

    made agents

--- agent.py
+++ agent.py
@@ -1,30 +1,74 @@
-from tictactoegame import game 
+from random import shuffle
+from tictactoegame import Game 
 from abc import ABC, abstractmethod
 
 class Policy(ABC):
     @abstractmethod
-    def predict(self):
+    def predict(self, game: Game, checkvalue: str) -> int:
         pass
+
+class Random(Policy):
+    def __init__(self) -> None:
+        super().__init__()
+        
+    def predict(self, game: Game, checkvalue: str) -> int:
+        free_states = game.free_states(checkvalue)
+        shuffle(free_states)
+        return free_states[0]
+             
+
+        
+
+
 
 class Greedy(Policy):
     def __init__(self) -> None:
         super().__init__()
-
         self._init_valuefunc()
 
     def _init_valuefunc(self):
         valuefunc = {}
         for i in range(3**9):
-            b = game(i)
+            b = Game(i)
             if b._islegal():
                 if b.wins("x"):
                     valuefunc[i] = 1
                 else:
                     valuefunc[i] = 0
+
     def update(self):
         pass
-    def predict(self):
+
+    @abstractmethod
+    def predict(self, game: Game, mark: str) -> int:
         pass
-class agent:
-    def __init__(self, policy: Policy):
+
+class Agent:
+    def __init__(self, policy: Policy, mark: str):
+        assert mark in ["x",  "o"]
+        self.mark = mark
+
+        if self.mark == "x":
+            self.myturns = 0
+        else:
+            self.myturns = 1
         self.policy = policy
+
+class Match:
+    def __init__(self, agent_1: Agent, agent_2: Agent):
+        self.agent_1 = agent_1
+        self.agent_2 = agent_2
+
+    def play(self, game: Game):
+        won = False
+        turn = 0
+        while won is False:
+            moving_agent = [self.agent_1, self.agent_2][turn % 2]
+            x_or_o = ["x", "o"][turn % 2]
+            game.update_state(moving_agent.predict(game, x_or_o))
+            game.draw()
+            turn += 1
+            if game.wins("x") or game.wins("o"):
+                won = True
+                game.draw()
+
--- tictactoegame.py
+++ tictactoegame.py
@@ -1,16 +1,20 @@
-from typing import Callable
 
-class game:
+from os import makedev
+
+
+class Game:
     def __init__(self, num = 0):
         self.board = self._decimal_to_tic_tac_toe(num)
-        
-    def place(self, cross: bool, row: int, col: int):
+
+    def place(self, checkvalue: str, row: int, col: int):
         if self.board[row][col] != " ":
             raise Exception(f"{row}, {col} already occupied")
-        if cross:
-            self.board[row][col] = "x"
-        else:
-            self.board[row][col] = "o"
+        self.board[row][col] = checkvalue
+
+    def unplace(self, row: int, col: int):
+        if self.board[row][col] == " ":
+            raise Exception(f"{row}, {col} not occupied")
+        self.board[row][col] = ""
 
     def _count(self, checkvalue: str):
         res = 0
@@ -23,6 +27,37 @@ class game:
     
     def wins(self, checkvalue: str):
         return self._count_rows(checkvalue) + self._count_cols(checkvalue) + self._count_diagonals(checkvalue) > 0
+
+
+    def _get_state_num(self):
+        """Gets the state number belonging to the current state"""
+        res = 0
+        index = 0
+        for i in reversed(range(3)):
+            for j in reversed(range(3)):
+                if self.board[i][j] == "x":
+                    res += 3 ** index 
+                if self.board[i][j] == "o":
+                    res += 2 * 3 ** index
+                index += 1
+        return res
+
+    def free_states(self, checkvalue: str):
+        res = []
+        free_squares = self._free_squares()
+        for row,col in free_squares:
+            self.place(checkvalue, row, col)
+            res.append(self._get_state_num())
+            self.unplace(row, col)
+        return res
+
+    def _free_squares(self):
+        free = []
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == " ":
+                    free.append((i,j))
+        return free
 
     def _islegal(self):
         xs, os = self._count("x"), self._count("o")
@@ -128,6 +163,9 @@ class game:
 
         return num_diags
 
+    def update_state(self, state: int):
+        self.board = self._decimal_to_tic_tac_toe(state)
+
     def _decimal_to_tic_tac_toe(self, index):
         if index < 0 or index >= 3**9:
             raise ValueError("Index out of range. Must be between 0 and 19682.")
@@ -144,15 +182,13 @@ class game:
         for i in range(0, 9, 3):
             row = [symbol_map[ternary[j]] for j in range(i, i + 3)]
             board.append(row)
-        
         return board
-if __name__ == "__main__":
-    valuefunc = {}
-    for i in range(3**9):
-        b = game(i)
-        if b._islegal():
-            if b.wins("x"):
-                valuefunc[i] = 1
-            else:
-                valuefunc[i] = 0
 
+if __name__ == "__main__":
+    b = Game(33)
+    print("current:")
+    b.draw()
+    for free in b.free_states("x"):
+        print(free)
+        g = Game(free)
+        g.draw()
